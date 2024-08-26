5defaab18a4e2137ac59afb9489cc915c0c49cb1
Pooja Kekan <35685820+poojakekan@users.noreply.github.com>
2024-08-26 13:08:44 +0530

    second commit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { toggleCart } from '../redux/cartSlice';
+import { addToCart, removeFromCart, increaseQuantity, decreaseQuantity } from './cartSlice';
 
 const Cart = () => {
-  const isVisible = useSelector((state) => state.cart.isVisible);
+  const cartItems = useSelector(state => state.cart.items);
+  const totalQuantity = useSelector(state => state.cart.totalQuantity);
   const dispatch = useDispatch();
+
+  const handleAddToCart = (item) => {
+    dispatch(addToCart(item));
+  };
+
+  const handleRemoveFromCart = (id) => {
+    dispatch(removeFromCart(id));
+  };
+
+  const handleIncreaseQuantity = (id) => {
+    dispatch(increaseQuantity(id));
+  };
+
+  const handleDecreaseQuantity = (id) => {
+    dispatch(decreaseQuantity(id));
+  };
 
   return (
     <div>
-      <button onClick={() => dispatch(toggleCart())}>
-        {isVisible ? 'Hide Cart' : 'Show Cart'}
-      </button>
-      {isVisible && <div>Your cart items go here...</div>}
+      <h2>Cart ({totalQuantity} items)</h2>
+      <ul>
+        {cartItems.map(item => (
+          <li key={item.id}>
+            {item.title} - ${item.price} x {item.quantity}
+            <button onClick={() => handleIncreaseQuantity(item.id)}>+</button>
+            <button onClick={() => handleDecreaseQuantity(item.id)}>-</button>
+            <button onClick={() => handleRemoveFromCart(item.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default Cart;
-   
--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -1,14 +1,71 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  items: [],
+  totalQuantity: 0,
+};
+
 const cartSlice = createSlice({
   name: 'cart',
-  initialState: { isVisible: false },
+  initialState,
   reducers: {
-    toggleCart(state) {
-      state.isVisible = !state.isVisible;
+    addToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find(item => item.id === newItem.id);
+
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          title: newItem.title,
+          quantity: 1,
+          price: newItem.price,
+          totalPrice: newItem.price,
+        });
+        state.totalQuantity++;
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice += newItem.price;
+        state.totalQuantity++;
+      }
+    },
+    removeFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id);
+        state.totalQuantity--;
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+        state.totalQuantity--;
+      }
+    },
+    increaseQuantity(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+
+      if (existingItem) {
+        existingItem.quantity++;
+        existingItem.totalPrice += existingItem.price;
+        state.totalQuantity++;
+      }
+    },
+    decreaseQuantity(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id);
+        state.totalQuantity--;
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+        state.totalQuantity--;
+      }
     },
   },
 });
 
-export const { toggleCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, increaseQuantity, decreaseQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
